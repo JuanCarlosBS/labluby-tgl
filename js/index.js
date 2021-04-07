988285cc51b2fd277ac4a6fde7fbc72740739fdc
Juan Carlos <juanrov_@hotmail.com>
2021-04-07 16:35:26 -0300

    feat: start js session and users functions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,52 @@
 numbers = []
+users = [{
+    name: 'admin',
+    email: 'admin@admin.com',
+    password: '123'
+}]
 
+function logIn() {
+    const email = document.querySelector('#email').value
+    const password = document.querySelect('#password').value
+
+    users.forEach((item) => {
+        if (item['email'] === email && item['password'] === password) {
+            return true
+        }
+    });
+}
+
+function createUser() {
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
+
+    users.forEach((item) => {
+        if (item['email'] === email ) {
+            return false
+        }
+
+        users.put({
+            name: name,
+            email: email,
+            password: password
+        })
+        
+        return true
+    });
+
+}
+
+function forgotPassword() {
+    const email = document.querySelector('#email').value
+    users.forEach(item => {
+        if (item['email'] === email ) {
+            return true
+            const password = document.querySelector('#password').value
+            item['password'] = password
+        }
+    })
+}
 function completeGame(game) {
     if (game) {
         Math.floor(Math.random() * 61)
