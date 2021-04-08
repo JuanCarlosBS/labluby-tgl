acf709f49d9452a36481ee8a49ea0ea1fb7641a3
Juan Carlos <juanrov_@hotmail.com>
2021-04-08 12:03:30 -0300

    test: testing getGames

--- js/index.js
+++ js/index.js
@@ -1,81 +1,22 @@
-numbers = []
-users = [{
-    name: 'admin',
-    email: 'admin@admin.com',
-    password: '123'
-}]
+(() => {
+    'use strict';
 
-function logIn() {
-    const email = document.querySelector('#email').value
-    const password = document.querySelect('#password').value
+    var $button = document.querySelector('.btnSubmit');
 
-    users.forEach((item) => {
-        if (item['email'] === email && item['password'] === password) {
-            return true
+    $button.addEventListener('click', getGames ,false);
+
+    function getGames() {
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', '/json/games.json');
+        ajax.send();
+        ajax.onreadystatechange = () => {
+            if(ajax.status === 200) {
+                var data = JSON.parse(ajax.responseText);
+                for (var i = 0; i < data.types.length; i++) {
+                        console.log(data.types[i])
+                }
+            }
         }
-    });
-}
-
-function createUser() {
-    const name = document.querySelector('#name').value
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
-
-    users.forEach((item) => {
-        if (item['email'] === email ) {
-            return false
-        }
-
-        users.put({
-            name: name,
-            email: email,
-            password: password
-        })
-        
-        return true
-    });
-
-}
-
-function forgotPassword() {
-    const email = document.querySelector('#email').value
-    users.forEach(item => {
-        if (item['email'] === email ) {
-            return true
-            const password = document.querySelector('#password').value
-            item['password'] = password
-        }
-    })
-}
-function completeGame(game) {
-    if (game) {
-        Math.floor(Math.random() * 61)
     }
 
-    if (game) {
-        Math.floor(Math.random() * 81)
-    }
-
-    if (game) {
-        Math.floor(Math.random() * 26)
-    }
-}
-
-function clearGame() {
-    while (numbers.length > 0) {
-        numbers.shift()
-    }
-}
-
-function addToCard() {
-    var removeButton = document.createElement('button')
-
-    removeButton.addEventListener('click', () => {
-        deleteCard(removeButton)
-    })
-}
-
-function deleteCard(removeButton) {
-    const deleteCard = removeButton.parentNode
-    deleteCard.parentNode.removeChild(deleteCard) 
-}
+})()
