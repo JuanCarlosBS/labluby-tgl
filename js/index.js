e399b01a63dff5088b723e59e7eea2d4c67c6811
Juan Carlos <juanrov_@hotmail.com>
2021-04-09 13:40:47 -0300

    feat: alter description by game

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@
     var $lotofacil = document.getElementById('lotofacil');
     var $megaSena = document.getElementById('mega-sena');
     var $lotomania = document.getElementById('lotomania');
+    var $number = document.querySelectorAll('.number');
     console.log($megaSena);
     $lotofacil.addEventListener('click', handleLotofacil, false);
     $megaSena.addEventListener('click', handleMegaSena, false);
@@ -27,41 +28,52 @@
     }    
 
     function handleMegaSena() {
-        const $numbers = document.querySelector('.numbers') 
+        const $descriptionGame  = document.querySelector('.description-game')
+        const $numbers = document.querySelector('.numbers')
         clearLabel($numbers)
+        clearLabel($descriptionGame)
+        $descriptionGame.innerHTML = '<h4>Fill your bet</h4>'
+        $descriptionGame.innerHTML += `<p>${megaSena.description}</p>`
         for(let i = 1; i <= megaSena.range; i++) {
             let label = i
             if( label < 10 ) 
                 label = `0${label}`
-            $numbers.innerHTML += `<button>${label}</button>`
+            $numbers.innerHTML += `<button class="number" id="${i}" value="${i}">${label}</button>`
         }
     }
 
     function handleLotofacil() {
-        const $numbers = document.querySelector('.numbers') 
+        const $descriptionGame  = document.querySelector('.description-game')
+        const $numbers = document.querySelector('.numbers')
         clearLabel($numbers)
+        clearLabel($descriptionGame)
+        $descriptionGame.innerHTML = '<h4>Fill your bet</h4>'
+        $descriptionGame.innerHTML += `<p>${lotofacil.description}</p>`
         for(let i = 1; i <= lotofacil.range; i++) {
             let label = i
             if( label < 10 ) 
                 label = `0${label}`
-            $numbers.innerHTML += `<button>${label}</button>`
+            $numbers.innerHTML += `<button class="number" id="${i}" value="${i}">${label}</button>`
         }
     }
 
     function handleLotomania() {
-        const $numbers = document.querySelector('.numbers') 
+        const $descriptionGame  = document.querySelector('.description-game')
+        const $numbers = document.querySelector('.numbers')
         clearLabel($numbers)
+        clearLabel($descriptionGame)
+        $descriptionGame.innerHTML = '<h4>Fill your bet</h4>'
+        $descriptionGame.innerHTML += `<p>${lotomania.description}</p>`
         for(let i = 1; i <= lotomania.range; i++) {
             let label = i
             if( label < 10 ) 
                 label = `0${label}`
-            $numbers.innerHTML += `<button>${label}</button>`
+            $numbers.innerHTML += `<button class="number" id="${i}" value="${i}">${label}</button>`
         }
     }
 
     function clearLabel(param) {
         return param.innerHTML = ''
     }
-
 
 })()
